3c48cbe3a7d47a4f36662bb95d85bdd5356f6d45
Ainhoa Alz <ainhoaalzola@gmail.com>
2024-05-02 23:02:56 +0200

    Fix bug: selection of color by default

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function(){
     const gridContainer = document.querySelector(".grid-container"); 
     const gridSizeSlider = document.querySelector("#gridSizeSlider");
     const gridSize = document.querySelector(".grid-size");
+    const colorPicker = document.querySelector("#colorPicker");
+    let currentColor = colorPicker.value || "#000000";
     
     function createGrid(size){
         gridContainer.innerHTML = "";
@@ -16,7 +18,9 @@ document.addEventListener("DOMContentLoaded", function(){
             gridContainer.appendChild(square); 
 
             square.addEventListener("mouseover", function(){
-                square.classList.add("hovered");
+                if (!square.style.backgroundColor || square.style.backgroundColor === currentColor) { // Aplicar nuevo color solo si el cuadrado no tiene un color asignado o ya tiene el color actual
+                    square.style.backgroundColor = currentColor;
+                }
             });
         }
         gridSize.textContent = `${size}x${size}`;
@@ -27,6 +31,11 @@ document.addEventListener("DOMContentLoaded", function(){
         createGrid(this.value);
 
     });
+    colorPicker.addEventListener('change', function() { // Usar el evento 'change' en lugar de 'click' para detectar cambios de color
+        currentColor = colorPicker.value || "#000000"; // Actualizar el color actualmente seleccionado o establecer el color por defecto
+    });
+
+    
     
 
 });
