9fc4f8116e6378e9dc90622ac979f67346791f00
Ainhoa Alz <ainhoaalzola@gmail.com>
2024-05-02 22:01:20 +0200

    Add grid size slider

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-
 document.addEventListener("DOMContentLoaded", function(){
     const gridContainer = document.querySelector(".grid-container"); 
-    for (let i=0; i<16*16;i++){
-        const square= document.createElement("div");
-        square.classList.add("square");
-        gridContainer.appendChild(square); 
+    const gridSizeSlider = document.querySelector("#gridSizeSlider");
+    const gridSize = document.querySelector(".grid-size");
+    
+    function createGrid(size){
+        gridContainer.innerHTML = "";
 
-    square.addEventListener("mouseover", function(){
-        square.classList.add("hovered")
-    });
+        const squareSize = 600 / size - 2; 
+
+        for (let i = 0; i < size * size; i++){
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
+            gridContainer.appendChild(square); 
+
+            square.addEventListener("mouseover", function(){
+                square.classList.add("hovered");
+            });
+        }
+        gridSize.textContent = `${size}x${size}`;
     }
-})
 
+    createGrid(gridSizeSlider.value);
+    gridSizeSlider.addEventListener('input', function() {
+        createGrid(this.value);
+
+    });
+    
+
+});
